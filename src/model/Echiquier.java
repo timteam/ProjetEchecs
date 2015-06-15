5bd6faf454b2fd3800ee487e6d09314c02f883ea
Timothée TRONCY <timothee.troncy@gmail.com>
2015-06-15 08:47:19 +0200

    ADD extends JFrame + folder organization
    Start OK

---
+++ src/model/Echiquier.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author timotheetroncy
+ */
+public class Echiquier {
+
+    public boolean isEchecEtMat() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void switchJoueur() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getMessage() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Couleur getColorCurrentPlayer() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/vue/ChessGameGUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vue;
+
+import controler.ChessGameControlers;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author timotheetroncy
+ */
+public class ChessGameGUI extends JFrame{
+
+
+    public ChessGameGUI(String jeu_déchec, ChessGameControlers chessGameControler, Dimension dim) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
